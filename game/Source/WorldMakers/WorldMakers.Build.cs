b20c80f124575febf24ada0333b43779d1ea998c
Juan Pablo Valderrama Pino <125180580+VladPhil92@users.noreply.github.com>
2026-09-15 16:02:49 -0500

    Pre-Unreal: link Enhanced Input module

--- game/Source/WorldMakers/WorldMakers.Build.cs
+++ game/Source/WorldMakers/WorldMakers.Build.cs
@@ -24,6 +24,7 @@ public class WorldMakers : ModuleRules
             "CoreUObject",
             "Engine",
             "InputCore",
+            "EnhancedInput",
             "PhysicsCore",
             "ProceduralMeshComponent",
             "Niagara",
